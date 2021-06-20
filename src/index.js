3af4c4b2060d240f3e4719bff6cfdb8c43d4b289
dannychi <chiqicheng@outlook.com>
2021-06-20 11:49:51 +0800

    beforeDestroy -> beforeUnmount

--- src/index.js
+++ src/index.js
@@ -1,75 +1,60 @@
-import Mixin from './mixin';
-import Logger from './logger';
-import Listener from './listener';
-import Emitter from './emitter';
-import SocketIO from 'socket.io-client';
+import Mixin from "./mixin";
+import Logger from "./logger";
+import Listener from "./listener";
+import Emitter from "./emitter";
+import SocketIO from "socket.io-client";
 
 export default class VueSocketIO {
+  /**
+   * lets take all resource
+   * @param io
+   * @param vuex
+   * @param debug
+   * @param options
+   */
+  constructor({ connection, vuex, debug, options }) {
+    Logger.debug = debug;
+    this.io = this.connect(connection, options);
+    this.emitter = new Emitter(vuex);
+    this.listener = new Listener(this.io, this.emitter);
+  }
 
-    /**
-     * lets take all resource
-     * @param io
-     * @param vuex
-     * @param debug
-     * @param options
-     */
-    constructor({connection, vuex, debug, options}){
+  /**
+   * Vue.js entry point
+   * @param Vue
+   */
+  install(Vue) {
+    const version = Number(Vue.version.split(".")[0]);
 
-        Logger.debug = debug;
-        this.io = this.connect(connection, options);
-        this.emitter = new Emitter(vuex);
-        this.listener = new Listener(this.io, this.emitter);
-
+    if (version >= 3) {
+      Vue.config.globalProperties.$socket = this.io;
+      Vue.config.globalProperties.$vueSocketIo = this;
+    } else {
+      Vue.prototype.$socket = this.io;
+      Vue.prototype.$vueSocketIo = this;
     }
 
-    /**
-     * Vue.js entry point
-     * @param Vue
-     */
-    install(Vue){
+    Vue.mixin(Mixin);
 
-        const version = Number(Vue.version.split('.')[0])
+    Logger.info("Vue-Socket.io plugin enabled: vue-", version);
+  }
 
-        if (version >= 3) {
-            Vue.config.globalProperties.$socket = this.io;
-            Vue.config.globalProperties.$vueSocketIo = this;
-        } else {
-            Vue.prototype.$socket = this.io;
-            Vue.prototype.$vueSocketIo = this;
-        }
+  /**
+   * registering SocketIO instance
+   * @param connection
+   * @param options
+   */
+  connect(connection, options) {
+    if (connection && typeof connection === "object") {
+      Logger.info("Received socket.io-client instance");
 
-        Vue.mixin(Mixin);
+      return connection;
+    } else if (typeof connection === "string") {
+      Logger.info("Received connection string");
 
-        Logger.info('Vue-Socket.io plugin enabled');
-
+      return (this.io = SocketIO(connection, options));
+    } else {
+      throw new Error("Unsupported connection type");
     }
-
-
-    /**
-     * registering SocketIO instance
-     * @param connection
-     * @param options
-     */
-    connect(connection, options){
-
-        if(connection && typeof connection === 'object'){
-
-            Logger.info('Received socket.io-client instance');
-
-            return connection;
-
-        } else if(typeof connection === 'string'){
-
-            Logger.info('Received connection string');
-
-            return this.io = SocketIO(connection, options);
-
-        } else {
-
-            throw new Error('Unsupported connection type');
-
-        }
-
-    }
-
+  }
 }
--- src/mixin.js
+++ src/mixin.js
@@ -1,56 +1,44 @@
 export default {
+  /**
+   *  Assign runtime callbacks
+   */
+  beforeCreate() {
+    if (!this.sockets) this.sockets = {};
 
-    /**
-     *  Assign runtime callbacks
-     */
-    beforeCreate(){
+    this.sockets.subscribe = (event, callback) => {
+      this.$vueSocketIo.emitter.addListener(event, callback, this);
+    };
 
-        if(!this.sockets) this.sockets = {};
+    this.sockets.unsubscribe = (event) => {
+      this.$vueSocketIo.emitter.removeListener(event, this);
+    };
+  },
 
-        this.sockets.subscribe = (event, callback) => {
-            this.$vueSocketIo.emitter.addListener(event, callback, this);
-        };
-
-        this.sockets.unsubscribe = (event) => {
-            this.$vueSocketIo.emitter.removeListener(event, this);
-        };
-
-    },
-
-    /**
-     * Register all socket events
-     */
-    mounted(){
-
-        if(this.$options.sockets){
-
-            Object.keys(this.$options.sockets).forEach(event => {
-
-                if(event !== 'subscribe' && event !== 'unsubscribe') {
-                    this.$vueSocketIo.emitter.addListener(event, this.$options.sockets[event], this);
-                }
-
-            });
-
+  /**
+   * Register all socket events
+   */
+  mounted() {
+    if (this.$options.sockets) {
+      Object.keys(this.$options.sockets).forEach((event) => {
+        if (event !== "subscribe" && event !== "unsubscribe") {
+          this.$vueSocketIo.emitter.addListener(
+            event,
+            this.$options.sockets[event],
+            this
+          );
         }
-
-    },
-
-    /**
-     * unsubscribe when component unmounting
-     */
-    beforeDestroy(){
-
-        if(this.$options.sockets){
-
-            Object.keys(this.$options.sockets).forEach(event => {
-
-                this.$vueSocketIo.emitter.removeListener(event, this);
-
-            });
-
-        }
-
+      });
     }
+  },
 
-}
+  /**
+   * unsubscribe when component unmounting
+   */
+  beforeUnmount() {
+    if (this.$options.sockets) {
+      Object.keys(this.$options.sockets).forEach((event) => {
+        this.$vueSocketIo.emitter.removeListener(event, this);
+      });
+    }
+  },
+};
